e20cf56f689905eb7a6243819195f1a867d0b609
Jonathan Pastor <jonathancmoa@gmail.com>
2019-10-14 11:51:47 +0200

    'continuous' -> 'contiguous'

--- lib/refrepo/gen/oar-properties.rb
+++ lib/refrepo/gen/oar-properties.rb
@@ -1197,7 +1197,7 @@ def extract_clusters_description(clusters, site_name, options, data_hierarchy, s
         gpu_idx += 1
       end
 
-      if core_numbering == 'continuous'
+      if core_numbering == 'contiguous'
         cpuset = 0
       end
 
@@ -1269,7 +1269,7 @@ def extract_clusters_description(clusters, site_name, options, data_hierarchy, s
           ############################################
           # (2-d) Associate a cpuset to each core
           ############################################
-          if core_numbering == 'continuous'
+          if core_numbering == 'contiguous'
             row[:cpuset] = cpuset
           else
             # CPUSETs starts at 0
@@ -1297,7 +1297,7 @@ def extract_clusters_description(clusters, site_name, options, data_hierarchy, s
 
           core_idx += 1
 
-          if core_numbering == 'continuous'
+          if core_numbering == 'contiguous'
             cpuset += 1
           end
 
